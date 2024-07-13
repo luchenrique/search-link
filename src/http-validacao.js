3a085b10c54fbfd3dda5320277cc97f0bd835090
Lucas Henrique <lucas.barbosa3680@gmail.com>
2024-07-12 21:43:22 -0300

    docs: documentando a arquivo de validação

--- src/http-validacao.js
+++ src/http-validacao.js
@@ -1,14 +1,14 @@
-function extraiLinks(arrLinks) {
-  return arrLinks.map((objetoLink) => Object.values(objetoLink).join());
+function extraiLinks(arrLinks) { // Função que extrai os links do texto
+  return arrLinks.map((objetoLink) => Object.values(objetoLink).join()); // Retorna um array com os links encontrados
 }
 
-async function checaStatus(listaURLs) {
-  const arrStatus = await Promise.all(
-    listaURLs.map(async (url) => {
-      try {
+async function checaStatus(listaURLs) { // Função que verifica o status dos links
+  const arrStatus = await Promise.all( // Retorna um array com os status dos links
+    listaURLs.map(async (url) => { // Mapeia a lista de links
+      try { // Tenta fazer a requisição
         const response = await fetch(url);
         return response.status;
-      } catch (erro) {
+      } catch (erro) { // Caso ocorra um erro, retorna a mensagem de erro
         return manejaErro(erro);
       }
     })
@@ -16,20 +16,21 @@ async function checaStatus(listaURLs) {
   return arrStatus;
 }
 
-function manejaErro(erro) {
-  if (erro.cause.code === "ENOTFOUND"){
+// Tratamento de erro
+function manejaErro(erro) { 
+  if (erro.cause.code === "ENOTFOUND"){ 
     return 'Link quebrado';
   } else {
-    return 'Ocorreu algum erro';
+    return 'Ocorreu algum erro'; 
   }
 }
 
-export default async function listaValidada(listaDeLinks) {
-  const links = (extraiLinks = extraiLinks(listaDeLinks));
-  const status = await checaStatus(links);
+export default async function listaValidada(listaDeLinks) { // Função que retorna a lista de links validados
+  const links = (extraiLinks = extraiLinks(listaDeLinks)); 
+  const status = await checaStatus(links); // Retorna o status dos links
 
-  return listaDeLinks.map((objeto, index) => ({
-    ...objeto,
+  return listaDeLinks.map((objeto, index) => ({ // Retorna um array com os links e seus status
+    ...objeto, // Retorna o objeto com o link
     status: status[index],
   }));
 }
