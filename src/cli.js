fc8c3de811bc50e0a502ecf183535e64a99f0b8f
Lucas Henrique <lucas.barbosa3680@gmail.com>
2024-07-12 21:36:10 -0300

    docs: documentando o arquivo cli.js

--- src/cli.js
+++ src/cli.js
@@ -3,17 +3,17 @@ import fs from "fs";
 import chalk from "chalk"; 
 import listaValidada from "./http-validacao.js";
 
-const caminho = process.argv;
+const caminho = process.argv; // Pega o caminho do arquivo ou diretório
 
 async function imprimeLista(valida, resultado, identificador = '') {
 
-    if (valida){
+    if (valida){ // Caso --valida seja verdadeiro será impresso a lista validada + resultado
       console.log(
         chalk.yellow("Lista Validada"),
         chalk.black.bgGreen(identificador),
         await listaValidada(resultado));        
     }else{
-      console.log(
+      console.log( // Caso --valida seja falso será impresso a lista de links + resultado
         chalk.yellow("Lista de links"),
         chalk.black.bgGreen(identificador),
         resultado);        
@@ -25,24 +25,24 @@ async function processaTexto(argumentos) {
     const valida = argumentos[3] === '--valida';
 
     try{
-        fs.lstatSync(caminho);
+        fs.lstatSync(caminho); // Verifica a existencia do caminhho informado
     } catch (erro) {
-        if(erro.code === 'ENOENT'){
+        if(erro.code === 'ENOENT'){ // Mensagem de erro caso o arquivo ou diretório não seja encontrado
             console.log(chalk.red('Arquivo ou diretório não encontrado'));
             return;
         }
     }
 
-    if (fs.lstatSync(caminho).isFile()){
-        const resultado = await pegaArquivo(argumentos[2]);
-        imprimeLista(valida, resultado);
-    } else if (fs.lstatSync(caminho).isDirectory()){
-        const arquivos = await fs.promises.readdir(caminho)
-        arquivos.forEach(async (nomeDeArquivo) => {
-            const lista = await pegaArquivo(`${caminho}/${nomeDeArquivo}`)
+    if (fs.lstatSync(caminho).isFile()){ // Verifica se o caminho é um arquivo
+        const resultado = await pegaArquivo(argumentos[2]); //
+        imprimeLista(valida, resultado); // Imprime a lista de links
+    } else if (fs.lstatSync(caminho).isDirectory()){ // Verifica se o caminho é um diretório
+        const arquivos = await fs.promises.readdir(caminho) 
+        arquivos.forEach(async (nomeDeArquivo) => { 
+            const lista = await pegaArquivo(`${caminho}/${nomeDeArquivo}`) 
             imprimeLista(valida, lista, nomeDeArquivo);
         })
     }
 }
 
-processaTexto(caminho); 
+processaTexto(caminho); // Chama a função processaTexto passando o caminho do arquivo ou diretório
