0ab070220129416ca479bac2f4ab6d3daa72dedf
Lucas Henrique <lucas.barbosa3680@gmail.com>
2024-07-12 21:09:56 -0300

    docs: documentando o arquivo index.js

--- src/index.js
+++ src/index.js
@@ -1,23 +1,24 @@
-import fs from 'fs';
-import chalk from 'chalk';
+import fs from 'fs'; // biblioteca nativa do node para ler arquivos
+import chalk from 'chalk'; // biblioteca para colorir o terminal
 
-function extraiLinks(texto) {
-    const regex = /\[([^[\]]*?)\]\((https?:\/\/[^\s?#.].[^\s]*)\)/gm;
+function extraiLinks(texto) { // Função que extrai os links do texto
+    const regex = /\[([^[\]]*?)\]\((https?:\/\/[^\s?#.].[^\s]*)\)/gm; // Define uma expressão regular para capturar links em formato markdown
     const capturas = [...texto.matchAll(regex)];
     const resultados = capturas.map(captura => ({[captura[1]]: [captura[2]]}))
-    return resultados.length !== 0 ? resultados : chalk.red('não há links no arquivo');
+    return resultados.length !== 0 ? resultados : chalk.red('não há links no arquivo'); // Retorna com array com os links encontrados ou uma mensagem de erro em vermelho
 }
 
-function trataErro(erro){
-    console.log(erro);
-    throw new Error(chalk.red(erro.code, 'não há arquivo no caminho informado'));
+function trataErro(erro){ // Função que trata os erros
+    console.log(erro); // Imprime o erro no console
+    throw new Error(chalk.red(erro.code, 'não há arquivo no caminho informado')); // Outra exceção, caso não encontre o arquivo
 }
 
-async function pegaArquivo(caminhoDoArquivo){
+async function pegaArquivo(caminhoDoArquivo){ // Função que lê o arquivo
     try {
       const enconding = "utf-8";
+      // Faz a leitra do arquivo
       const texto = await fs.promises.readFile(caminhoDoArquivo, enconding);
-      return extraiLinks(texto);
+      return extraiLinks(texto); // Extrái os links do texto que foi lido
     } catch (erro) {
       trataErro(erro);
     } 
